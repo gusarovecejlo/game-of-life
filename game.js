1d437468923996cb3bda3025b7fcc286cec55835
gusarovecejlo <qweasz132>
2018-06-17 00:37:53 +0300

    Initial commit

---
+++ game.js
@@ -0,0 +1,15 @@
+const Grid = require('./grid')
+
+
+class Game {
+    constructor(gridSize) {
+        this.grid = new Grid(gridSize, true)
+    }
+
+    play() {
+        return this.grid.compute().render()
+    }
+
+}
+
+module.exports = Game
